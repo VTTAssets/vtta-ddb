62f0c905221d4d812bc9cda71a0246b5c04cd2e4
Sebastian Will <vttassets@gmail.com>
2021-07-14 17:59:14 +0200

    Updated in-game release notes

--- src/setup/showReleaseNotes.js
+++ src/setup/showReleaseNotes.js
@@ -24,32 +24,34 @@ export default async () => {
     `<h1>Release Notes: ${game.modules.get(config.module.name).data.title} v${
       game.modules.get(config.module.name).data.version
     }</h1>
-      
-      <p><b>Private Beta</b> Thanks for participating in this private beta, exclusively for you wonderful Patreons. What are we testing in this first step? Stability!</p>
+      <h2>Private Beta</h2>
+      <p>Thanks for participating in this private beta, exclusively for you wonderful Patreons. What are we testing in this first step? <b>Stability!</b></p>
       <p>Mainly the stability of the server environment - there quite some changes been made to the modules, too, but the main work was done server-side and I need to make sure everything is working 
       as planned. Please focus your feedback on</p>
       <ul>
-      <li><strong>Response times</strong> How long does it take to import a single monster? Does the response times from parsing a page and going to the next when batch-importing feels alright? <strong>Please note that bringing both browser tabs (D&amp;D Beyond and FVTT) into the foreground speeds up processing significantly!</strong>.</li>
-      <li><strong>Functionality</strong> Does it work at all? If not, was is not working?</li>
+      <li><b>Response times</b> How long does it take to import a single monster? Does the response times from parsing a page and going to the next when batch-importing feels alright? <strong>Please note that bringing both browser tabs (D&amp;D Beyond and FVTT) into the foreground speeds up processing significantly!</strong>.</li>
+      <li><b>Functionality</b> Does it work at all? If not, was is not working?</li>
       </ul>
 
-      <p>Things currently not in focus are</p>
-      <ul>
-      <li><strong>Inconsistencies in the output</strong>, e.g. maps missing or incomplete, monsters being incorrectly parsed, Journal Entries missing...</li>
-      <li><strong>The Forge is not working</strong> - <i>*shrug*</i> I aim for compatibility, but not at the very first step. Let's get vanilla Foundry working first</li>
-      </ul>
+      <p>Please report your experiences on Discord. I will be available for questions and feedback.</p>
       
-      <h3>Features: New and/or improved</h3>
+      <h2>Features: New and/or improved</h2>
        <ul>
           <li><b>All Sourcebooks/ Adventures supported</b> - that's a milestone for sure</li>
           <li><b>Better Token generation</b> - If you have <strong>VTTA Tokens</strong> installed, you will see way better auto-generated tokens from now on. You will need to re-import to update the token images though.</li>
           <li><b>Easier (and more stable) FVTT connection</b> - Connect to FVTT just by clicking on the browser icon, it will automatically try to connect. <i>A click saved is a click saved</i></li>
-          <li><b>Environment selection</b> - Mainly targetted to developers, but relevant to this beta, too: Select the parser environment from within the extension. While in this beta, the <strong>STAGING environment</strong> at <strong>vtta.dev</strong> will be used exclusively. If you are experiencing issues, change the setting to vtta.dev and reconnect to Foundry</li>
+          <li><b>Environment selection</b> - This will be relevant for developers contributing changes, as we will have our own playground to destroy stuff without affecting anyone really</li>
        </ul>
 
-       <p>
-       <strong>Have fun!</strong>
-       </p>
+       <h2>Known issue</h2>
+       <ul>
+        <li><b>Clicking on the browser extension's icon to connect to FVTT reloads the FVTT tab</b> and you are not being able to connect at all.<br/>
+             <b>Workaround:</b> Go to the browser's extension management page (chrome://extensions), search for the VTTA extension and hit the little "Reload"-icon. Refresh the FVTT tab and try to connect.</li>
+
+
+       </ul>
+
+       <h2>Have fun!</h2>
       <hr />
          `,
     {
